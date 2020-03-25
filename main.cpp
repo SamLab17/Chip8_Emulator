ecf97511068fa5397f53eba59b8dc8a4caa311a9
SamLab17 <samlaberge@live.com>
2020-03-25 14:51:29 -0600

    Changed to simple view

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@
 
 int main(int argc, char **argv) {
     auto *c = new Controller("C8Programs/PONG");
-    c->start(true);
+    c->start(false);
     // Once emulation has ended (window was closed), we will return here
     return 0;
 }
